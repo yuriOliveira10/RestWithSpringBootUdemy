a3593db1e7b6690cbbbb4fc8283bc5f02583a6bb
Yuri <yuri.oliveirab@gmail.com>
2020-07-03 23:06:29 -0300

    Continuos Integration With Travis CI, Docker Hub, Amazon ECR and ECS

---
+++ 18 RestWithSpringBootUdemy Amazon/RestWithSpringBootUdemy/src/main/java/br/com/bezeyur/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.bezeyur.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.bezeyur"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(apiInfo());
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("RESTfull Api With Spring boot 2.1.3",
+				"Descricao", 
+				"V1",
+				"Termos de Servico",
+				new Contact("Yuri", "yuri.com", "yuri@email.com"),
+				"Licenca",
+				"url licenca",
+				Collections.emptyList());
+	}
+
+}
